477a045308d557e475cd380f17678ee1a52ae264
esztermazi <49866977+esztermazi@users.noreply.github.com>
2020-04-14 22:52:35 +0200

    Add ThemeContext component

---
+++ src/static/util/AppTheme.js
@@ -0,0 +1,19 @@
+const AppTheme = {
+  mordor: {
+    variant: 'warning',
+    bodyBackground: '#d3d3d3',
+    textColor: 'gold',
+  },
+  shire: {
+    variant: 'succes',
+    bodyBackground: 'rgba(41, 241, 195, 1)',
+    textColor: '#fff',
+  },
+  gondor: {
+    variant: 'light',
+    bodyBackground: '#fff',
+    textColor: 'grey',
+  },
+};
+
+export default AppTheme;
