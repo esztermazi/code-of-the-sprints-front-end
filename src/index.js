d6c8a9682810a734c56ced17a7568f1aa6180a2c
esztermazi <49866977+esztermazi@users.noreply.github.com>
2020-05-22 12:29:51 +0200

    Move shapes by 1 coordinate by removing React.StrictMode

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.Fragment>
     <LoginProvider>
       <App />
     </LoginProvider>
-  </React.StrictMode>,
+  </React.Fragment>,
   document.getElementById('root')
 );
